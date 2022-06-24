e51cf602fbb651059fe1da41966440f9d90a4c05
The-Emper0r <100057955+The-Emper0r@users.noreply.github.com>
2022-06-24 02:37:46 +0100

    Update models.py

--- I4G000842WW3/blog/models.py
+++ I4G000842WW3/blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+from django.utils import timezone
 
-from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = models.ForeignKey(User, on_delete=models.CASCADE)
-    Created_Date = models.DateTimeField()
-    Published_date = models.DateTimeField()
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_Date = models.DateTimeField(auto_now_add=True)
+    Published_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
